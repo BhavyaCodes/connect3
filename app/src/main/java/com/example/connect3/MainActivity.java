9c3efff39265a753fd1cb5685079d36909cd391b
Juggernaut9 <xbhavyatomar@gmail.com>
2020-03-30 14:57:55 +0530

    -Updated rotation of X and O from 900 to 1080 degree which fixed inversion of image. -Added image corresponding to active player bottom right of screen

--- app/src/main/java/com/example/connect3/MainActivity.java
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -15,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2,2,2,2,2,2,2,2,2};
 
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    int activePlayer = 0;
+    int activePlayer = 1;
     boolean gameActive = true;
     int draw=0;
 
     public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
+        ImageView turnPreview = (ImageView) findViewById(R.id.turnPreview);
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
         if (gameState[tappedCounter] == 2 && gameActive) {      //check if box is empty and game is active
 
@@ -35,17 +36,20 @@ public class MainActivity extends AppCompatActivity {
                 Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
                 playAgainButton.setVisibility(view.VISIBLE);
                 textView.setText("Draw");
+                turnPreview.setVisibility(View.INVISIBLE);
                 draw=0;
             }
 
             if (activePlayer == 0) {
                 counter.setImageResource(R.drawable.o);
+                turnPreview.setImageResource(R.drawable.x);
                 activePlayer = 1;
             } else {
                 counter.setImageResource(R.drawable.x);
+                turnPreview.setImageResource(R.drawable.o);
                 activePlayer = 0;
             }
-            counter.animate().translationYBy(1500).rotationBy(900).setDuration(300);
+            counter.animate().translationYBy(1500).rotationBy(1080).setDuration(300);
 
             for (int[] winningPosition : winningPositions) {    // win condition true
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
@@ -62,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                     textView.setVisibility(View.VISIBLE);
                     Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
                     playAgainButton.setVisibility(View.VISIBLE);
+                    turnPreview.setVisibility(View.INVISIBLE);
                 }
             }
         }
@@ -79,7 +84,10 @@ public class MainActivity extends AppCompatActivity {
         for (int j=0; j<gameState.length; j++){
             gameState[j]=2;
         }
-        activePlayer = 0;
+        ImageView turnPreview = (ImageView) findViewById(R.id.turnPreview);
+        turnPreview.setVisibility(View.VISIBLE);
+        turnPreview.setImageResource(R.drawable.x);
+        activePlayer = 1;
         gameActive = true;
         draw=0;
     }
